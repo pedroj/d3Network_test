68df69582ce0b296f8cfb8bd4a6bcd83275ecf17
Pedro Jordano <pedroj@me.com>
2013-09-09 22:43:13 +0200

    Added the Nava Correhuelas dataset and graph

--- d3test.R
+++ d3test.R
@@ -2,15 +2,18 @@
 # Pedro Jordano. Sevilla. 8-11 Sept 2014l.
 devtools::install_github("d3Network", "christophergandrud", ref = "sankey")
 
+library(d3Network)
 # Load energy projection data
 library(RCurl)
+
 # Path to the json data file.
 # I converted the initial weighted adjacency matrix for HR to a json 
 # data file. First I vectorized the matrix; then I re-labelled the 
 # node names and links to match a json input file format.
 # This needs to be imporved by implementing a function to auto re-code
 # the input matrix.
-URL<- "file:///Users/pedro/Documents/Working/~RCode/MyRCode/networks/d3Network/hr.json"
+# This is the Hato Raton dataset.
+URL<- "file:///Users/pedro/Documents/Working/~RCode/MyRCode/networks/d3Network_test/hr.json"
 
 hr<- getURL(URL, ssl.verifypeer = FALSE)
 hrLinks <- JSONtoDF(jsonStr = hr, array = "links")
@@ -21,3 +24,14 @@ d3Sankey(Links = hrLinks, Nodes = hrNodes, Source = "source",
          Target = "target", Value = "value", NodeID = "name"
          , fontsize = 12, nodeWidth = 30, file = "TestHR.html")
 
+# This is the Nava Correhuelas dataset.
+URL<- "file:///Users/pedro/Documents/Working/~RCode/MyRCode/networks/d3Network_test/nch.json"
+
+nch<- getURL(URL, ssl.verifypeer = FALSE)
+nchLinks <- JSONtoDF(jsonStr = nch, array = "links")
+nchNodes <- JSONtoDF(jsonStr = nch, array = "nodes")
+# Plot
+# Open the TestHR.html in the browser.
+d3Sankey(Links = nchLinks, Nodes = nchNodes, Source = "source",
+         Target = "target", Value = "value", NodeID = "name"
+         , fontsize = 12, nodeWidth = 30, file = "TestNCH.html")
